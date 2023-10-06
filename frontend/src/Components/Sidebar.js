69e46b9045f6b54bed55ee14696cfcfedf135608
Rajdeep Karmakar <78910001+deep-karm@users.noreply.github.com>
2023-10-06 12:52:46 +0530

    Add files via upload
    Added Components

---
+++ frontend/src/Components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			{/* Your sidebar content goes here */}
+			<ul>
+				<li>Dashboard</li>
+				<li>Profile</li>
+				<li>Settings</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Sidebar;
---
+++ frontend/src/Components/TopNavigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TopNavigation = () => {
+	return (
+		<div className="top-navigation">
+			{/* Your navigation content goes here */}
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+	);
+};
+
+export default TopNavigation;
