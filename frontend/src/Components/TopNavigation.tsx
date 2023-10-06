7ac1980e8d01bc313a619e96ce19d01e6d115b2f
rheasan <shreyaschavan228a@gmail.com>
2023-10-07 00:05:37 +0530

    add styles to TopNavigation

--- frontend/src/Components/TopNavigation.tsx
+++ frontend/src/Components/TopNavigation.tsx
@@ -1,17 +1,15 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../assets/logo.jpg";
 // import logo from '../logo.png';
 const TopNavigation = () => {
 	return (
-		<div className="top-navigation bg-blue-500">
-			{/* Your navigation content goes here */}
-			<ul>
-				<li>Home</li>
-				<li>About</li>
-				<li>Contact</li>
-				{/* <div className="logo">
-					<img src={logo} alt="My Logo" />
-				</div> */}
-			</ul>
+		<div className="top-navigation bg-blue-500 flex flex-row h-12 justify-between items-center gap-4">
+			<div className="flex flex-row gap-4 items-center">
+				<Image src={logo} alt="iitp-logo" width={48} height={48}/>
+				<h1 className="text-xl">ERP System</h1>
+			</div>
+			<button className="text-red p-2">Login</button>
 		</div>
 	);
 };
