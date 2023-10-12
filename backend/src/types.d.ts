25be20b3b6f1a09b444ad3088fae8108a4827d43
rheasan <shreyaschavan228a@gmail.com>
2023-10-12 12:27:17 +0530

    Ready! Set! Go! but rust

--- backend/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-import {job, z_new_process, z_process} from "./processes"
-import z from "zod";
-
-type Job = z.infer<typeof job>;
-type process = z.infer<typeof z_process>;
-type new_process = z.infer<typeof z_new_process>;
