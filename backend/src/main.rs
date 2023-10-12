056649e31f21dc17ffbc4088ae942e73bce42f94
rheasan <shreyaschavan228a@gmail.com>
2023-10-12 13:01:54 +0530

    setup axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(say_hello));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("Running on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn say_hello() -> &'static str {
+    "Hello, world!"
 }
