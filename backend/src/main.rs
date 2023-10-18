eca0c0d72cb62841281ed15ee9c41c2510ba7500
rheasan <shreyaschavan228a@gmail.com>
2023-10-19 00:52:47 +0530

    deploying to fl0 try: 1

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,13 +5,14 @@ pub mod process;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
+	let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+	let port = port.parse::<u16>().unwrap();
     let app = Router::new()
     .route("/", get(say_hello))
     .route("/process", get(process::get_processes))
     .route("/process", post(process::create_process));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
     println!("Running on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
