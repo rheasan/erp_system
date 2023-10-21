e0d63769cca29d630fa32410e675cabee568a9c1
rheasan <shreyaschavan228a@gmail.com>
2023-10-22 00:46:44 +0530

    add homepage

--- frontend/src/components/TopNavigation.tsx
+++ frontend/src/components/TopNavigation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import logo from "../assets/logo.jpg";
 // import logo from '../logo.png';
 const TopNavigation = () => {
@@ -7,7 +8,7 @@ const TopNavigation = () => {
 		<div className="top-navigation bg-blue-500 flex flex-row h-12 justify-between items-center gap-4">
 			<div className="flex flex-row gap-4 items-center">
 				<Image src={logo} alt="iitp-logo" width={48} height={48}/>
-				<h1 className="text-xl">ERP System</h1>
+				<Link href="/" className="text-xl">ERP System</Link>
 			</div>
 			<button className="text-red p-2">Login</button>
 		</div>
